be5ed5d625b0cd01955914a46caa80ad690a3295
Samuel Legrain <samuel.legrain@bstorm.be>
2023-01-11 12:21:44 +0100

    Ajoutez des fichiers projet.

---
+++ Demo-DAL/Mapper/Mapper.cs
@@ -0,0 +1,27 @@
+﻿using Demo_DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_DAL
+{
+    static class Mapper
+    {
+        public static Client ToClient(this IDataRecord record)
+        {
+            if (record is null) return null;
+            return new Client()
+            {
+                idClient = (int)record[nameof(Client.idClient)],
+                nom = (string)record[nameof(Client.nom)],
+                prenom = (string)record[nameof(Client.prenom)],
+                email = (string)record[nameof(Client.email)],
+                pass = "********",
+                adresse = (record[nameof(Client.adresse)] is DBNull) ? null : (string)record[nameof(Client.adresse)]
+            };
+        }
+    }
+}
---
+++ Demo-DAL/Services/ClientService.cs
@@ -0,0 +1,53 @@
+﻿using Demo_DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_DAL.Services
+{
+    class ClientService
+    {
+        private string ConnectionString { get; set; } = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Theatre-DB;Integrated Security=True";
+        public IEnumerable<Client> Get()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using(SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [idClient], [nom], [prenom], [email], [adresse] FROM [Client]";
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return reader.ToClient();
+                        }
+                    }
+                }
+            }
+        }
+
+        public Client Get(int id)
+        {
+            return null;
+        }
+
+        public int Insert(Client entity)
+        {
+            return 0;
+        }
+
+        public bool Update(int id, Client entity)
+        {
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            return false;
+        }
+    }
+}
