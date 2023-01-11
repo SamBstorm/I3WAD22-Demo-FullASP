9e045737fb3393cd216bdf64c042f17e7b9f4c02
Samuel Legrain <samuel.legrain@bstorm.be>
2023-01-11 16:49:50 +0100

    Add Repository not-finished

---
+++ Demo-Common/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_Common.Repositories
+{
+    interface IRepository<TEntity, TId>
+    {
+        IEnumerable<TEntity> Get();
+        TEntity Get(TId id);
+        TId Insert(TEntity entity);
+        bool Update(TId id, TEntity entity);
+        bool Delete(TId id);
+    }
+}
